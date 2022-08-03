1d39d26da94f9de712dfa5c76a23f66ecc97e435
sudharsan <sudharsan.devarajan@htcinc.com>
2022-08-03 18:43:51 +0530

    Initial Commit

---
+++ EmpList/EmpListApp.swift
@@ -0,0 +1,20 @@
+//
+//  EmpListApp.swift
+//  EmpList
+//
+//  Created by htcuser on 03/08/22.
+//
+
+import SwiftUI
+
+@main
+struct EmpListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
